3eea5e0876148213a6bf271cef05651fbbe8f796
paulodiff <paulo.difficiliora@gmail.com>
2022-09-13 08:09:15 +0200

    1 file modified

--- script1.js
+++ script1.js
@@ -129,3 +129,22 @@ function onTakePhoto2ButtonClick() {
       console.error(error);
     });
 }
+
+function drawCanvas(canvas, img, resize) {
+  console.log('drawCanvas', img.width, img.height, resize);
+
+  if(resize)  {
+    canvas.width = img.width;
+    canvas.height = img.height;
+  } else {
+    canvas.width = getComputedStyle(canvas).width.split('px')[0];
+    canvas.height = getComputedStyle(canvas).height.split('px')[0];
+  }
+
+  let ratio  = Math.min(canvas.width / img.width, canvas.height / img.height);
+  let x = (canvas.width - img.width * ratio) / 2;
+  let y = (canvas.height - img.height * ratio) / 2;
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+  canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
+      x, y, img.width * ratio, img.height * ratio);
+}
